2a555ffd54a5c0b1a1f233204ee1c7c3ad2fcf0f
Alex <alex@konstankino.com>
2023-07-03 05:28:04 +0300

    Added saving and getting todos from localStorage

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -2,16 +2,28 @@ import * as React from "react";
 
 import TodoList from "./TodoList";
 import NewTodo from "./NewTodo";
-import { useDispatch } from "react-redux";
-
-const { useContext } = React;
+import { useDispatch, useSelector } from "react-redux";
+import { getTodos } from "./Redux/selectors";
 
 export default function Todo() {
+  const todos = useSelector(getTodos);
   const dispatch = useDispatch();
   function handleClearButtonClick(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
     dispatch({ type: "CLEAR_TODOS" });
   }
+
+  React.useEffect(() => {
+    const todos: string = window.localStorage.getItem("todos") || "";
+    dispatch({
+      type: "SET_TODOS",
+      payload: JSON.parse(todos)
+    });
+  }, []);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <>
